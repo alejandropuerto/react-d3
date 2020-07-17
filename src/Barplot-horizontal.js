6f7cc2bd7342055b33e95a32c899046bbd187e9e
Alejandro Puerto <puertoalejandro@outlook.com>
2020-07-17 13:06:04 -0500

    fixed, showing heights by characters for StarWars API

--- src/Barplot-horizontal.js
+++ src/Barplot-horizontal.js
@@ -6,11 +6,11 @@ function GraphHorizontal(props){
 
   useEffect(() => {
 
-    const margin = {top:30, right:30, bottom:70, left:60};
+    const margin = {top:30, right:30, bottom:70, left:90};
     const width = 460 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
     const max_height = d3.max(props.data.map(d => d.height).map(Number)); //Get the max height mapping strings to numbers
-
+    console.log(max_height);
     d3.select(canvas.current).select('svg').remove();
 
     // to filter only the first 10 elements from props.data
@@ -29,7 +29,7 @@ function GraphHorizontal(props){
     const x = d3
       .scaleLinear()
       .range([0, width])
-      .domain([0, max_height]);
+      .domain([0, max_height])
     //agregando eje de X al canvas
     svg
       .append('g')
@@ -41,28 +41,27 @@ function GraphHorizontal(props){
 
     const y = d3
       .scaleBand()
-      .domain(props.data.map((d) => d.name)
+      .domain(props.data.map((d) => d.name))
       .range([0, height])
-      .padding(0.2);
+      .padding(0.1)
     //esto agrega el eje y al canvas
     svg.append("g").call(d3.axisLeft(y))
 
     //Barras
     svg
       .selectAll("myRect")
-      .data(props.data) //change props.data for topData when only want the first 10 elements
+      .data(props.data)
       .enter()
       .append("rect")
-      .attr("x", (d) => {
-        return y(d.height);
-      })
+      //.attr("x", (d) => { return x(d.height); })
+      .attr("x", x(0))
       .attr("y", function (d) {
-          return x(d.name);
+          return y(d.name);
       })
       .attr("width", (d) => {
-        return height - y(d.height);
+        return x(d.height);
       })
-      .attr("height", x.bandwidth())
+      .attr("height", y.bandwidth())
       .attr("fill", "#69b3a2");
   }, [props.data]);
 
